60fad8c563caa8c8c3c7334da0222048ec1d1edc
Niyi Onabanjo <46007245+ineyoh@users.noreply.github.com>
2019-11-26 18:56:35 -0500

    Update ui.r

--- ui.r
+++ ui.r
@@ -2,71 +2,116 @@ library(shiny)
 library(shinydashboard)
 library(shinyjs)
 library(leaflet)
-library(ggmap)
 
-# ==== function allowing geolocalisation
-jsCode <- '
-shinyjs.geoloc = function() {
-    navigator.geolocation.getCurrentPosition(onSuccess, onError);
-    function onError (err) {
-        Shiny.onInputChange("geolocation", false);
-    }
-    function onSuccess (position) {
-        setTimeout(function () {
-            var coords = position.coords;
-            console.log(coords.latitude + ", " + coords.longitude);
-            Shiny.onInputChange("geolocation", true);
-            Shiny.onInputChange("lat", coords.latitude);
-            Shiny.onInputChange("long", coords.longitude);
-        }, 5)
-    }
-};
-'
+header <- dashboardHeader(title = "Montreal Statistics", 
+                          dropdownMenu(type = "messages",
+                                                   messageItem(
+                                                     from = "Census Department",
+                                                     message = "Conceptualize the census app"
+                                                   ),
+                                                   messageItem(
+                                                     from = "New User",
+                                                     message = "How do I register?",
+                                                     icon = icon("question"),
+                                                     time = "15:00"
+                                                   ),
+                                                   messageItem(
+                                                     from = "Support",
+                                                     message = "The new server is ready.",
+                                                     icon = icon("life-ring"),
+                                                     time = "2014-12-01"
+                                                   )
+                       ),
+                       dropdownMenu(type = "notifications",
+                                    notificationItem(
+                                      text = "5 new users today",
+                                      icon("users")
+                                    ),
+                                    notificationItem(
+                                      text = "12 items delivered",
+                                      icon("truck"),
+                                      status = "success"
+                                    ),
+                                    notificationItem(
+                                      text = "Server load at 86%",
+                                      icon = icon("exclamation-triangle"),
+                                      status = "warning"
+                                    )
+                       ),
+                       dropdownMenu(type = "tasks", badgeStatus = "success",
+                                    taskItem(value = 90, color = "green", "Documentation"),
+                                    taskItem(value = 17, color = "aqua", "Project X"),
+                                    taskItem(value = 75, color = "yellow", "Server deployment"),
+                                    taskItem(value = 80, color = "red", "Overall project")
+                                    )
+)
 
-register_google(key = "AIzaSyCheEjvViqFWET60ZFk_SYueQX3Rk9z5SE", write = TRUE)
+sidebar <- dashboardSidebar(skin = "black",
+                               sidebarMenu(
+                                 menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+                                 menuItem("Widgets", tabName = "widgets", icon = icon("th"), 
+                                          badgeLabel = "new", badgeColor = "green"),
+                                 menuItem("Montreal map", tabName = "maps"),
+                                 menuItem("Montreal gallery", tabName = "gallery")
+                                 )
+                             )
 
-shinyServer(function(input, output){
-  
-  output$histogram <- renderPlot({
-    hist(rnorm(input$test))
-  })
-  
-  #output$value <- renderText({input$mapInput})
-  
-    output$map <- renderLeaflet({
-      # Get latitude and longitude
-    if(input$target_zone=="Bamako"){
-      ZOOM=2
-      LAT=0
-      LONG=0
-    }else{
-      target_pos=geocode(input$target_zone)
-      LAT=target_pos$lat
-      LONG=target_pos$lon
-      ZOOM=20
-    }
-      # Plot it!
-      leaflet() %>%
-      addTiles() %>%
-      setView(lng=LONG, lat=LAT, zoom=ZOOM ) %>%
-      addProviderTiles("OpenStreetMap_Mapnik") 
-      })
-        
-    
-    # Find geolocalisation coordinates when user clicks
-    observeEvent(input$geoloc, {
-      js$geoloc()
-    })
-    
-   
-    # zoom on the corresponding area
-    observe({
-      if(!is.null(input$lat)){
-        map <- leafletProxy("map")
-        dist <- 0.2
-        lat <- input$lat
-        lng <- input$long
-        map %>% fitBounds(lng - dist, lat - dist, lng + dist, lat + dist)
-      }
-    })
-})
+body <- dashboardBody(
+    tabItems(
+      
+      tabItem(tabName = "dashboard",
+              h1("Census Details"),
+              valueBox(10 * 2, "Area Codes Counted", icon = icon("thumbs-up"), color = "green"),
+              valueBox(10 * 200, "People paid", icon = icon("credit-card"),  color = "yellow"),
+              #tags$br(),  
+              valueBox(5 * 2, "Progress", icon = icon("list"), color = "teal")
+      ),
+      
+      tabItem(tabName = "widgets",
+              h1("Census Statistics"),
+              box(title = "Input", status = "warning", solidHeader = TRUE,
+                  "Select a Number", collapsible = TRUE,
+                  sliderInput("test", "Population in Montreal", 1, 100, 50),
+                  textInput("text", "Text input:")),
+              box(title = "Input", background = "olive", solidHeader = TRUE,
+                  "View the Output", collapsible = TRUE, 
+                  plotOutput("histogram")),
+              h4("Download census Data for Montreal"),
+              box(absolutePanel(top=745, left=300, actionButton("url", "Download Report", class="btn btn-primary", 
+                                                            onclick="window.open('https://www12.statcan.gc.ca/census-recensement/2016
+                                                            /dp-pd/prof/details/download-telecharger/comp/GetFile.cfm?Lang=E&FILETYPE=CSV&GEONO=065')")))
+              
+              # Create a data frame.
+              #print(is.data.frame(count))
+      ),
+      
+      tabItem(tabName = "maps",
+              # Tell shiny we will use some Javascript
+              useShinyjs(),
+              extendShinyjs(text = jsCode),
+              absolutePanel(top=70, left=300, textInput("target_zone", "Enter a postal code, or name of a place" , "Montreal")),
+              br(),
+              absolutePanel(top=140, left=300, leafletOutput("map", width = "1200px", height = "600px")),
+              #one button and one map
+              absolutePanel(top=745, left=300, actionButton("geoloc", "My Location", class="btn btn-primary", onClick="shinyjs.geoloc()"))
+      ),
+      
+    tabItem(tabName = "gallery",
+            h1("Montreal Gallery"),
+            tags$img(height =300,
+                         width = 500,  
+                         src = "https://www.mtlblog.com/uploads/284903_03ccde64d3656b89cef0db7f0cdaf20d6cfc70dc.jpg_facebook.jpg"),
+            tags$img(height =300, 
+                     width = 500,  
+                         src = "https://www.mtlblog.com/u/2019/09/10/9bfab897fee050ccc74bde25aa7dd8bc.jpg_1200x630.jpg")
+            )
+    )
+)
+
+#Add all the content into a dashboardPage
+dashboardPage(skin = "green", 
+               header,
+               sidebar,
+               body
+              )
+
